ea2a7445cb032073ecc6960422f19c091cfd03aa
Zuxelator <zuxel87@mail.ru>
2021-04-27 19:40:05 +0400

    2. Каркас шахматной доски [#497124]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -21,7 +21,15 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        return true;
+        boolean rsl = true;
+        for (int i = 0; i < figures.length; i++) {
+            for (int j = 0; j < steps.length; j++) {
+                if((figures[i].position()).equals(steps[j])) {
+                    throw new OccupiedCellException();
+                }
+            }
+        }
+        return rsl;
     }
 
     public void clean() {
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,18 +1,59 @@
 package ru.job4j.chess;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
+
+import java.util.Arrays;
 
 public class LogicTest {
 
-    @Ignore
-    @Test
-    public void move()
+
+    @Test(expected = OccupiedCellException.class)
+    public void moveOccupiedCellException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
+        logic.add(new PawnBlack(Cell.H6));
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
     }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void moveFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void impossibleMoveException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.H7);
+    }
+
+    @Test
+    public void blackBishopPositionTest() {
+        BishopBlack bb = new BishopBlack(Cell.C8);
+        Assert.assertEquals(bb.position(), Cell.C8);
+    }
+
+    @Test
+    public void blackBishopCopyTest() {
+        BishopBlack bb = new BishopBlack(Cell.C8);
+        Figure copyOfBishop = bb.copy(Cell.A6);
+        Assert.assertEquals(copyOfBishop.position(), Cell.A6);
+    }
+    @Test
+    public void blackBishopWayTest() {
+        BishopBlack bb = new BishopBlack(Cell.C1);
+        Cell[] arr = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Assert.assertTrue(Arrays.equals(arr, bb.way(Cell.G5)));
+    }
 }
+
